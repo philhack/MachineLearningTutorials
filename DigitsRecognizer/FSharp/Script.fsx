c376b840d14fc3f9a79bd76adf94196829066c24
Phil Hack <p.hack@marketinvoice.com>
2016-01-11 10:46:02 +0000

    added f# code to read in training data and cast it to observation type

--- DigitsRecognizer/FSharp/Script.fsx
+++ DigitsRecognizer/FSharp/Script.fsx
@@ -1,12 +1,19 @@
-﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
-// for more guidance on F# programming.
-
-#load "Library1.fs"
+﻿#load "Library1.fs"
 open FSharp
+open System.IO
 
-// Define your library scripting code here
-let square x = x * x
-let sumOfSquares n = [1..n] |> List.map square |> List.sum
+type Observation = { Label:string; Pixels: int[] }
 
-let answer = sumOfSquares 100
+let toObservation (csvData:string) =
+    let columns = csvData.Split(',')
+    let label = columns.[0]
+    let pixels = columns.[1..] |> Array.map int
+    { Label = label; Pixels = pixels}
 
+let reader path =
+    let data = File.ReadAllLines path
+    data.[1..]
+    |> Array.map toObservation
+
+let trainingPath = @"C:\dev\InnovationDay\MachineLearning\DigitsRecognizer\Data\trainingsample.csv"
+let trainingData = reader trainingPath
