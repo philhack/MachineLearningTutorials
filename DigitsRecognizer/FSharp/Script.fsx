23f327d3d0a9b99426f3123a7129b98c58a001b1
Phil Hack <p.hack@marketinvoice.com>
2016-01-11 10:31:25 +0000

    added initial fsx file

---
+++ DigitsRecognizer/FSharp/Script.fsx
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+#load "Library1.fs"
+open FSharp
+
+// Define your library scripting code here
+let square x = x * x
+let sumOfSquares n = [1..n] |> List.map square |> List.sum
+
+let answer = sumOfSquares 100
+
