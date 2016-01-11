fba0e6d58a2135b11eff844d734688bc3e07b55a
Phil Hack <p.hack@marketinvoice.com>
2016-01-11 12:30:11 +0000

    added code to read in the spam sms dataset

--- SpamChecker/SpamChecker/Script.fsx
+++ SpamChecker/SpamChecker/Script.fsx
@@ -1,6 +1,21 @@
-﻿type DocType =
+﻿open System.IO
+
+type DocType =
     | Ham
     | Spam
+
+
+let parseDocType (label:string) =
+    match label with
+    | "ham" -> Ham
+    | "spam" -> Spam
+    | _ -> failwith "Unknown label"
+
+let parseLine (line:string) = 
+    let split = line.Split('\t')
+    let label = split.[0] |> parseDocType
+    let message = split.[1]
+    (label, message)
 
 let identify (example:DocType*string) = 
     let docType,content = example
@@ -8,3 +23,9 @@ let identify (example:DocType*string) =
     | Ham -> printfn "'%s' is ham" content
     | Spam -> printfn "'%s' is spam" content
 
+let fileName = "SMSSpamCollection"
+let path = __SOURCE_DIRECTORY__ + @"..\..\Data\" + fileName
+
+let dataset = 
+    File.ReadAllLines path
+    |> Array.map parseLine
